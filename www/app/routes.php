5385f1ff4abf5cc9c3728510fb53e70484e2678a
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-08-05 21:04:47 -0300

    wip: setting basic of asking

--- www/app/routes.php
+++ www/app/routes.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
 use Danilocgsilva\ClassToSqlSchemaScript\FieldScriptSpitter;
+use Danilocgsilva\ClassToSqlSchemaScript\TableScriptSpitter;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -20,15 +21,43 @@ return function (App $app) {
     $app->get('/', function (Request $request, Response $response) {
         $queryStringsArray = $request->getQueryParams();
 
-        if ($tableName = $queryStringsArray['database']) {
-            $databaseScriptSpitter = new DatabaseScriptSpitter($tableName);
+        if (isset($queryStringsArray['database'])) {
+            $databaseScriptSpitter = new DatabaseScriptSpitter($queryStringsArray['database']);
             
             $response->getBody()->write(
                 $databaseScriptSpitter->getScript()
             );
-            return $response;;
+            return $response;
         }
 
-        return "You must choose if you want to generate a database or a table sql script.";
+        if (isset($queryStringsArray['table'])) {
+            $tableScriptSpitter = new TableScriptSpitter($queryStringsArray['table']);
+
+            // if (isset($queryStringsArray['id'])) {
+            //     $tableScriptSpitter->addField(
+            //         (new FieldScriptSpitter($queryStringsArray['id']))
+            //         ->setType("INT")
+            //         ->setUnsigned()
+            //         ->setAutoIncrement()
+            //         ->setNotNull()
+            //         ->setPrimaryKey()
+            //     );
+            // }
+
+            if () {
+
+            }
+
+            $response->getBody()->write(
+                str_replace("\n", "<br />", $tableScriptSpitter->getScript()) 
+            );
+
+            return $response;
+        }
+
+        $response->getBody()->write(
+            "You must choose if you want to generate a database or a table sql script."
+        );
+        return $response;
     });
 };
